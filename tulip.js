1bb54a7bd2f6200889ce9b15eabca9bd72e8f30c
miguel <miguel.aristizabalp.col@upb.edu.co>
2026-05-08 23:42:18 -0500

    update

--- tulip.js
+++ tulip.js
@@ -3,7 +3,7 @@ window.onload = () => {
     document.body.classList.remove("not-loaded");
   }, 1000);
 
-  const poem = "Siempre voy a estar para ti. Me importas muchísimo y me encanta estar contigo. Incluso en los momentos difíciles, sigues siendo alguien que quiero tener cerca. Te amo";
+  const poem = "Siempre voy a estar para ti. Me importas muchísimo y me encanta estar contigo. Y sobretodo en los momentos dificiles nunca voy a dejar de estar a tu lado. Te amo infinitamente";
   const element = document.getElementById("poem-text");
 
   let i = 0;
